d9465ed95f5cdaa488aea5542a8545f1d106bdfd
Josh Hays <jphays@gmail.com>
2015-02-10 08:43:36 -0800

    Moved ADSR function to effects.js.

--- scenes/flash.js
+++ scenes/flash.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var Color = require('color');
+var effects = require('../effects');
 var palettes = require('../palettes');
 
 var palette = palettes.triadicScheme({ h: _.random(360), s: 100, v: 80 });
@@ -42,62 +43,11 @@ function render(state)
             pixel.lastChangeTime = state.time;
         }
 
-        pixel.color = envelope(pixel.lastChangeTime, state,
+        pixel.color = effects.envelope(pixel.lastChangeTime, state,
             { color: palette[pixel.colorIndex].clone() });
     });
 
     return pixels.map(function(pixel) { return pixel.color; });
-
-}
-
-// An ADSR (attack/decay/sustain/release) envelope for a color.
-function envelope(startTime, state, options)
-{
-
-    options = _.defaults(options, {
-        startColor: "black",
-        attackColor: "white",
-        color: "white",
-        releaseColor: "black",
-        attackTime: 100,
-        decayTime: 250,
-        sustainTime: 400,
-        releaseTime: 300
-    });
-
-    var timeSinceStart = state.time - startTime;
-
-    var decayStart = options.attackTime;
-    var sustainStart = decayStart + options.decayTime;
-    var releaseStart = sustainStart + options.sustainTime;
-    var releaseEnd = releaseStart + options.releaseTime;
-
-    if (timeSinceStart < decayStart)
-    {
-        return Color(options.startColor).mix(
-                    Color(options.attackColor),
-                    timeSinceStart / options.attackTime);
-    }
-    else if (timeSinceStart < sustainStart)
-    {
-        return Color(options.attackColor).mix(
-                    Color(options.color),
-                    (timeSinceStart - decayStart) / options.decayTime);
-    }
-    else if (timeSinceStart < releaseStart)
-    {
-        return Color(options.color);
-    }
-    else if (timeSinceStart < releaseEnd)
-    {
-        return Color(options.color).mix(
-                    Color(options.releaseColor),
-                    (timeSinceStart - releaseStart) / options.releaseTime);
-    }
-    else
-    {
-        return Color(options.releaseColor);
-    }
 
 }
 
