501fde3ad9c832178c329cb34307f80ce20a7ef3
Josh Hays <jphays@gmail.com>
2015-02-15 22:38:27 -0800

    Randomize flash palette on init.

--- scenes/flash.js
+++ scenes/flash.js
@@ -3,21 +3,23 @@ var Color = require('color');
 var effects = require('../effects');
 var palettes = require('../palettes');
 
-var palette = palettes.triadicScheme({ h: _.random(360), s: 100, v: 80 });
-
+var palette;
 var pixels = [];
 var changeInterval = 1200;
 
 function init(options)
 {
+    pixels = [];
+    palette = palettes.triadicScheme({ h: _.random(360), s: 100, v: 80 });
+
     for (var i = 0; i < options.ledCount; i++)
     {
-        pixels[i] = {
+        pixels.push({
             colorIndex: palette.length - 1,
             color: Color("black"),
             changeTime: 5 * i,
             lastChangeTime: 0
-        };
+        });
     }
 }
 
